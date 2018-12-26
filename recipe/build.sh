10553dc2efa57c74552221dee01fde6af2d492fd
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-26 14:46:47 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ export CFLAGS="-I$PREFIX/include:$CFLAGS"
 export LDFLAGS="-L$PREFIX/lib:$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+ls -lrt $PREFIX/lib
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
