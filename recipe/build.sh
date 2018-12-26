fc87bd38cc687a23e5fd3cf0027431560aea11a7
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-26 16:04:08 +0100

    Trying dynamic linking

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,11 @@
 export CFLAGS="-I$PREFIX/include:$CFLAGS" 
 export LDFLAGS="-L$PREFIX/lib:$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$PREFIX/lib"
+export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ls -lrt $PREFIX/lib
 ./configure --prefix $PREFIX
 make install
 #Small test
 echo "main = putStr \"smalltest\"" > Main.hs
-ghc -fasm -v5 -o smalltest Main.hs 
+ghc -dynamic -fasm -v5 -o smalltest Main.hs 
 ./smalltest
