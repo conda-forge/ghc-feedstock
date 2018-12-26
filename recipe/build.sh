da4600578568b217e4f6ae3c86356b404d56e406
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-26 20:48:27 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 echo $PREFIX/lib
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 echo "$CFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LDFLAGS="-Wl,-L$PREFIX/lib -Wl,--verbose $LDFLAGS"
 echo "$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
@@ -10,9 +10,9 @@ ls -lrt $PREFIX/lib
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
-echo "ldconfig start"
-ldconfig -p
-echo "ldconfig end"
+#echo "ldconfig start"
+#ldconfig -p
+#echo "ldconfig end"
 echo "main = putStr \"smalltest\"" > Main.hs
-ghc -fasm -v5 -o smalltest Main.hs 
+ghc -fasm -o smalltest Main.hs 
 ./smalltest
