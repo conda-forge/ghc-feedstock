6578560e6b524eac818b4934e8a4a909bae4fb9d
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-26 15:35:37 +0100

    Made libraries path more restrictive

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,9 @@
 export CFLAGS="-I$PREFIX/include:$CFLAGS" 
 export LDFLAGS="-L$PREFIX/lib:$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+export LIBRARY_PATH="$PREFIX/lib"
 ls -lrt $PREFIX/lib
-./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+./configure --prefix $PREFIX
 make install
 #Small test
 echo "main = putStr \"smalltest\"" > Main.hs
