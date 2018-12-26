f587fa908858106be1a52fd9b9576fa204f65b47
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-26 22:09:06 +0100

    Passing env variables directly to configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,14 @@
 echo $PREFIX/lib
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 echo "$CFLAGS"
-export LDFLAGS="-Wl,-L$PREFIX/lib -Wl,--verbose $LDFLAGS"
+export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
 echo "$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ls -lrt $PREFIX/lib
 echo "Build_prefix/lib"
 ls -lrt $BUILD_PREFIX/lib
-./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib LDFLAGS=$LDFLAGS CFLAGS=$CFLAGS
 make install
 #Small test
 #echo "ldconfig start"
