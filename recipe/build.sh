cbe8a6336365dc079f8df1e4b54d2444c2bb7d8d
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-28 15:49:05 +0100

    Saving time

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,14 @@
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
-echo $PREFIX/lib
-export CFLAGS="-I$PREFIX/include $CFLAGS"
-echo "$CFLAGS"
-export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
-echo "$LDFLAGS"
+#export CFLAGS="-I$PREFIX/include $CFLAGS"
+#export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-ls -lrt $PREFIX/lib
-echo "Build_prefix/lib"
-ls -lrt $BUILD_PREFIX/lib
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
-#echo "ldconfig start"
-#ldconfig -v
-#echo "ldconfig end"
-#echo "gcc7ldconfig start"
-#x86_64-conda_cos6-linux-gnu-ldconfig -v
-#echo "gcc7ldconfig end"
-#ldd --version ldd
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
    ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $BUILD_PREFIX/bin/ld
 fi
-#echo "which ld"
-#which ld
-echo "main = putStr \"smalltest\"" > Main.hs
-#ghc -O0 -threaded -L$PREFIX/lib -L/usr/lib -L/usr/lib64 -fasm -o smalltest Main.hs 
-ghc -v -O0 -threaded -L$PREFIX/lib -fasm -o smalltest Main.hs 
-./smalltest
+#echo "main = putStr \"smalltest\"" > Main.hs
+#ghc -v -O0 -threaded -L$PREFIX/lib -fasm -o smalltest Main.hs 
+#./smalltest
