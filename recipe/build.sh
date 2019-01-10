121cc706b987e982f531c29b29ca3c0387044d8f
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-10 13:34:18 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make
 make install
