7985ffbee80644f413fa3348c90fefd1a3f09e38
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 23:22:17 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,13 +12,18 @@ ghc-pkg describe rts > rts.pkg
 perl -pi -e 's/$PREFIX\/lib\/ghc-8.2.2\/rts/$PREFIX\/lib\/ghc-8.2.2\/rts \$\{pkgroot\}\/../g' rts.pkg
 cat rts.pkg
 ghc-pkg update rts.pkg
+perl -pi -e 's/GhcStage1HcOpts=/GhcStage1HcOpts= -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcStage2HcOpts=/GhcStage2HcOpts= -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcStage3HcOpts=/GhcStage3HcOpts= -threaded /g' mk/config.mk.in
 ./configure --disable-ld-override --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+cat mk/config.mk
 cp mk/build.mk.sample mk/build.mk
 perl -pi -e 's/#BuildFlavour = quick/BuildFlavour = quick/g' mk/build.mk
+
 #./validate --build-only
 echo "Settings"
 cat settings
-make
+make 
 make install
 #Small test
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
