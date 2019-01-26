80a04d8c9a4837566e666a00fe81ce60a0c62ed9
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 21:30:29 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 export CC="x86_64-conda_cos6-linux-gnu-cc"
-export LD="x86_64-conda_cos6-linux-gnu-ld"
+export LD="x86_64-conda_cos6-linux-gnu-cd"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
