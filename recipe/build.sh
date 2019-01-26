59a129d04d1e363c91b78a4bae7511bb8502b5cc
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 14:46:33 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,15 @@
-export PWD=`pwd`
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PWD/includes:$PREFIX/include:$C_INCLUDE_PATH"
+export LD="x86_64-conda_cos6-linux-gnu-ld"
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 ls
 echo "includes"
 ls includes
-cp $PWD/includes/ghcversion.h $PREFIX/include
+#cp $PWD/includes/ghcversion.h $PREFIX/include
 make
+ls includes
 make install
 #Small test
 if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
