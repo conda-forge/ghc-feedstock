5237fe8ad57fc13680833c45aec300ef50f1273f
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 19:50:39 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,9 @@
+export CC="x86_64-conda_cos6-linux-gnu-cc"
+export LD="x86_64-conda_cos6-linux-gnu-ld"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PWD/includes:$PREFIX/include:$C_INCLUDE_PATH"
-export LD="x86_64-conda_cos6-linux-gnu-cc"
 export LDFLAGS=" -Wl,-L$PREFIX/lib -Wl,-lgmp $LDFLAGS"
 ghc-pkg recache
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
