81b51858b4ac148986698675bd16200ef08242db
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-26 22:35:36 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 export CC="x86_64-conda_cos6-linux-gnu-cc"
-export LD="x86_64-conda_cos6-linux-gnu-cd"
+export LD="x86_64-conda_cos6-linux-gnu-cc"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PWD/includes:$PREFIX/include:$C_INCLUDE_PATH"
-export LDFLAGS=" -Wl,-L$PREFIX/lib -Wl,-lgmp $LDFLAGS -Wl,-lpthread "
+export LDFLAGS=" -Wl,-L$PREFIX/lib -Wl,-lgmp -Wl,-lpthread $LDFLAGS "
+export LIBS=" -lgmp -lpthread $LIBS "
 ghc-pkg recache
 ghc-pkg describe rts
 ghc-pkg describe rts > rts.pkg
