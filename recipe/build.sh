8ae680fa79c854c8f4b4cad8cccb0222a3dd8851
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-27 14:08:03 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,11 +13,11 @@ ghc-pkg describe rts > rts.pkg
 perl -pi -e 's/$PREFIX\/lib\/ghc-8.2.2\/rts/$PREFIX\/lib\/ghc-8.2.2\/rts \$\{pkgroot\}\/../g' rts.pkg
 cat rts.pkg
 ghc-pkg update rts.pkg
-perl -pi -e 's/GhcRtsHcOpts=-O2 -fomit-frame-pointer -g/GhcRtsHcOpts=-O2 -fomit-frame-pointer -g -L\$PREFIX/lib -threaded /g' mk/config.mk.in
-perl -pi -e 's/GhcHcOpts=/GhcStage0HcOpts= -L\$PREFIX/lib -threaded /g' mk/config.mk.in
-perl -pi -e 's/GhcStage1HcOpts=/GhcStage1HcOpts= -L\$PREFIX/lib -threaded /g' mk/config.mk.in
-perl -pi -e 's/GhcStage2HcOpts=/GhcStage2HcOpts= -L\$PREFIX/lib -threaded /g' mk/config.mk.in
-perl -pi -e 's/GhcStage3HcOpts=/GhcStage3HcOpts= -L\$PREFIX/lib -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcRtsHcOpts=-O2 -fomit-frame-pointer -g/GhcRtsHcOpts=-O2 -fomit-frame-pointer -g -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcHcOpts=/GhcHcOpts= -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcStage1HcOpts=/GhcStage1HcOpts= -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcStage2HcOpts=/GhcStage2HcOpts= -threaded /g' mk/config.mk.in
+perl -pi -e 's/GhcStage3HcOpts=/GhcStage3HcOpts= -threaded /g' mk/config.mk.in
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 cat mk/config.mk
 cp mk/build.mk.sample mk/build.mk
