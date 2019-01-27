54984fb53e8bc87c91ef71538384fa0700e6b744
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-27 17:01:30 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,14 +13,14 @@ ghc-pkg describe rts > rts.pkg
 perl -pi -e 's/$PREFIX\/lib\/ghc-8.2.2\/rts/$PREFIX\/lib\/ghc-8.2.2\/rts \$\{pkgroot\}\/../g' rts.pkg
 cat rts.pkg
 ghc-pkg update rts.pkg
-#export CONF_GCC_LINKER_OPTS_STAGE0=" -L$PREFIX/lib -lgmp -lpthread "
-#export CONF_GCC_LINKER_OPTS_STAGE1=" -L$PREFIX/lib -lgmp -lpthread "
-#export CONF_GCC_LINKER_OPTS_STAGE2=" -L$PREFIX/lib -lgmp -lpthread "
-#export CONF_GCC_LINKER_OPTS_STAGE3=" -L$PREFIX/lib -lgmp -lpthread "
-export CONF_LD_LINKER_OPTS_STAGE0 =" -L$PREFIX/lib -lgmp -lpthread"
-export CONF_LD_LINKER_OPTS_STAGE1 =" -L$PREFIX/lib -lgmp -lpthread"
-export CONF_LD_LINKER_OPTS_STAGE2 =" -L$PREFIX/lib -lgmp -lpthread"
-export CONF_LD_LINKER_OPTS_STAGE3 =" -L$PREFIX/lib -lgmp -lpthread"
+export CONF_GCC_LINKER_OPTS_STAGE0=" -lgmp -lpthread "
+export CONF_GCC_LINKER_OPTS_STAGE1=" -lgmp -lpthread "
+export CONF_GCC_LINKER_OPTS_STAGE2=" -lgmp -lpthread "
+export CONF_GCC_LINKER_OPTS_STAGE3=" -lgmp -lpthread "
+export CONF_LD_LINKER_OPTS_STAGE0=" -lgmp -lpthread "
+export CONF_LD_LINKER_OPTS_STAGE1=" -lgmp -lpthread "
+export CONF_LD_LINKER_OPTS_STAGE2=" -lgmp -lpthread "
+export CONF_LD_LINKER_OPTS_STAGE3=" -lgmp -lpthread "
 export CONF_HC_OPTS_STAGE0=" -L$PREFIX/lib -lgmp -threaded -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc "
 export CONF_HC_OPTS_STAGE1=" -L$PREFIX/lib -lgmp -threaded -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc "
 export CONF_HC_OPTS_STAGE2=" -L$PREFIX/lib -lgmp -threaded -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc "
