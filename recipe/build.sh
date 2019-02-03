165d7ce887a2d4d66671618d362c06f2853b1c20
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-02-03 16:32:52 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,6 @@ export LD="x86_64-conda_cos6-linux-gnu-ld"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
+./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 make install
