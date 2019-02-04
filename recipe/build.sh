e43737db49f189f28a45ac870087f60d01967a87
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-02-04 01:27:47 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 export CC="x86_64-conda_cos6-linux-gnu-cc"
-export LD="x86_64-conda_cos6-linux-gnu-ld"
+export LD="x86_64-conda_cos6-linux-gnu-cc"
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib:$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib:$PREFIX/lib:$LIBRARY_PATH"
@@ -7,6 +7,7 @@ export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
 export LDFLAGS="-L$PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib -L$PREFIX/lib $LDFLAGS"
 echo "ls PREFIX"
 ls $PREFIX
-
+echo "ls PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib"
+ls $PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib
 ./configure --prefix=$PREFIX LDFLAGS=-L$PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/lib CPPFLAGS=-I$PREFIX/include CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 make install
