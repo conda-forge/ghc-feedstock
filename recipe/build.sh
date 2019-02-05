ae067ca6cd9887441a25b9b055461b67634fe2e4
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-02-06 00:52:45 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -55,6 +55,7 @@ echo "CONF_CC_OPTS_STAGE3 = -L$PREFIX/lib -lgmp -lpthread " >> mk/build.mk
 echo "SRC_HSC2HS_OPTS = -lgmp -lpthread " >> mk/build.mk
 #echo "libraries/integer-gmp_CONFIGURE_OPTS += --configure-option=LDFLAGS=\" -L$PREFIX/lib \" --ghc-options=\" -L$PREFIX/lib -lgmp -threaded -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc \"" >> mk/build.mk
 #echo "libraries/integer-gmp_CONFIGURE_OPTS = --with-gmp-includes=\"$PREFIX/include\" --with-gmp-libraries=\"$PREFIX/include\" " >> mk/build.mk
+echo "GhcLibHcOpts= -L$PREFIX/lib -lgmp -threaded -lgcc_s -pgmc x86_64-conda_cos6-linux-gnu-cc -pgml x86_64-conda_cos6-linux-gnu-cc " >> mk/build.mk
 cat mk/build.mk
 #./validate --build-only
 echo "Settings"
