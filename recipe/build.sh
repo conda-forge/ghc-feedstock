a66e12e6d010a30a8639bb0f4906a470752b593e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-02-18 23:30:33 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,13 @@ export LIBS=" -L$PREFIX/lib -lgmp -lpthread -lgcc_s $LIBS "
 #export PRIM_CFLAGS=" -fuse-ld=gold "
 export LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 ghc-pkg recache
-./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cpp CPPFLAGS=-I$PREFIX/include --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc AR=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar AS=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-as CFLAGS=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold # CONF_CPP_OPTS_STAGE0=-fuse-ld=gold CONF_CPP_OPTS_STAGE1=-fuse-ld=gold CONF_CPP_OPTS_STAGE2=-fuse-ld=gold CONF_CC_OPTS_STAGE0=-fuse-ld=gold CONF_CC_OPTS_STAGE1=-fuse-ld=gold CONF_CC_OPTS_STAGE2=-fuse-ld=gold PRIM_EXTRA_LIBRARIES=-L$PREFIX/lib PRIM_CFLAGS=-fuse-ld=gold --with-iconv-includes=$PREFIX/include" "--with-iconv-libraries=$PREFIX/lib
+./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cpp CPPFLAGS=-I$PREFIX/include --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc AR=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar AS=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-as CFLAGS=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold CPPFLAGS=
+# CONF_CPP_OPTS_STAGE0=-fuse-ld=gold CONF_CPP_OPTS_STAGE1=-fuse-ld=gold CONF_CPP_OPTS_STAGE2=-fuse-ld=gold CONF_CC_OPTS_STAGE0=-fuse-ld=gold CONF_CC_OPTS_STAGE1=-fuse-ld=gold CONF_CC_OPTS_STAGE2=-fuse-ld=gold PRIM_EXTRA_LIBRARIES=-L$PREFIX/lib PRIM_CFLAGS=-fuse-ld=gold --with-iconv-includes=$PREFIX/include" "--with-iconv-libraries=$PREFIX/lib
 cp mk/build.mk.sample mk/build.mk
 perl -pi -e 's/#BuildFlavour = quick\n/BuildFlavour = quick\n/' mk/build.mk
 echo "V=0" >> mk/build.mk
-echo "GhcLibHcOpts += -fPIC" >> mk/build.mk
-echo "GhcRtsHcOpts += -fPIC" >> mk/build.mk
+echo "GhcLibHcOpts += -fPIC -optl-fuse-ld=gold -optl-L$PREFIX/lib -lgcc -lgcc_s " >> mk/build.mk
+echo "GhcRtsHcOpts += -fPIC -optl-fuse-ld=gold -optl-L$PREFIX/lib -lgcc -lgcc_s " >> mk/build.mk
 echo "SplitObjs=NO" >> mk/build.mk
 echo "EXTRA_CC_OPTS += -std=gnu99" >> mk/build.mk
 echo "SRC_HC_OPTS = -O0 -H64m -optl-fuse-ld=gold -optl-L$PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0 -optl-L$PREFIX/lib -lgmp -threaded -lgcc -lgcc_s -pgmc $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc -pgml $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc " >> mk/build.mk
