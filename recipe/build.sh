33043945c0550003236d853ea7c710c75feb4c8b
step21 <step21@devtal.de>
2020-09-03 16:51:15 +0000

    modified build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,17 +3,20 @@ export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 #export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+echo $ARCH
+
 if [ $ARCH == "aarch64" ]; then
    echo "aarch64 detected"
 
 fi
 if [ $ARCH == "x86_64" ]; then
   echo "x86_64 detected"
-  export TARGET_PLATFORM="x86_64-unknown-linux"
+  export TARGETPLATFORM="x86_64-unknown-linux"
 fi
 uname -a
 which perl
 echo $TARGETPLATFORM
+cp $SRC_PREFIX/build.mk $BUILD_PREFIX/mk/
 ./boot
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make
