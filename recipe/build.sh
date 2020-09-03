05f6e4c10c2c807c00764f45858a6a4aa0bbbc60
step21 <step21@devtal.de>
2020-09-03 17:59:16 +0000

    unset test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
+unset host_alias
+unset build_alias
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 #export CFLAGS="-I$PREFIX/include $CFLAGS"
 #export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
@@ -16,7 +18,7 @@ fi
 uname -a
 which perl
 echo $TARGETPLATFORM
-cp $SRC_PREFIX/build.mk $BUILD_PREFIX/mk/
+cp build.mk $BUILD_PREFIX/mk/
 ./boot
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make
