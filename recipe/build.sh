f56117cbafb6d6c2fad307ee5d6999d07e18c0a1
step21 <step21@users.noreply.github.com>
2020-09-17 01:20:40 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,19 @@ export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 #export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-echo $ARCH
+# from eggzilla/ghc-feedstock
+echo "echo $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0"
+ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0
+ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld.gold $PREFIX/bin/ld.gold
+ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $PREFIX/bin/ld
+echo "ls $PREFIX/lib"
+ls $PREFIX/lib
+echo "$BUILD_PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0"
+ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0
 
+export LD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
+export LD_NO_GOLD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
+ghc-pkg recache
 if [ $ARCH == "aarch64" ]; then
    echo "aarch64 detected"
 
@@ -20,6 +31,8 @@ which perl
 echo $TARGETPLATFORM
 #cp build.mk $BUILD_PREFIX/mk/
 ./boot
+# from eggzilla
+# ./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$PREFIX/bin/cpp --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$PREFIX/bin/cc LD=$PREFIX/bin/cc AR=$PREFIX/bin/ar AS=$BUILD_PREFIX/bin/as CFLAGS=-fno-builtin CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold LD_NO_GOLD=$PREFIX/bin/cc
 #./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 if [ $ARCH == "64" ]; then
   ./configure --prefix=$PREFIX CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
@@ -27,6 +40,11 @@ fi
 if [ $ARCH == "aarch64" ]; then
   ./configure --prefix=$BUILD_PREFIX --with-gmp-includes=$BUILD_PREFIX/include --with-gmp-libraries=$BUILD_PREFIX/lib
 fi
+# from eggzilla
+cp mk/build.mk.sample mk/build.mk
+perl -pi -e 's/#BuildFlavour = quick\n/BuildFlavour = quickest\n/' mk/build.mk
+#echo "V=0" >> mk/build.mk
+#
 make
 make install
 if [[ -f "$LD" && ! $BUILD_PREFIX/bin/ld ]]; then
