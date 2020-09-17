c019b5f45e67113f3462927400aafc6ce631e690
step21 <step21@users.noreply.github.com>
2020-09-17 13:24:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,16 +6,17 @@ export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 # from eggzilla/ghc-feedstock
-#echo "echo $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0"
-#ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0
-ls $PREFIX/lib/gcc
-ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld.gold $PREFIX/bin/ld.gold
-ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $PREFIX/bin/ld
-echo "ls $PREFIX/lib"
-ls $PREFIX/lib
-echo "$BUILD_PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0"
-ls $PREFIX/lib/gcc/
-ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu
+echo "prefix"
+ls $PREFIX
+
+#ls $PREFIX/lib/gcc
+#ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld.gold $PREFIX/bin/ld.gold
+#ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $PREFIX/bin/ld
+#echo "ls $PREFIX/lib"
+#ls $PREFIX/lib
+#echo "$BUILD_PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0"
+#ls $PREFIX/lib/gcc/
+#ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu
 #ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0
 
 export LD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
