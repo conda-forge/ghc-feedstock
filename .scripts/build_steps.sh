0e3019beea36e33783d0270759477d22f54e1dd0
step21 <step21@devtal.de>
2020-10-05 13:33:18 +0200

    re-added modified build_steps

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -31,8 +31,12 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y epel-release ghc cabal-install
-
+#/usr/bin/sudo -n yum install -y epel-release ghc cabal-install
+/usr/bin/sudo -n yum install -y epel-release
+/usr/bin/sudo -n yum repolist
+/usr/bin/sudo -n yum install -y ghc cabal-install
+# apparently this requires a password but yum does not
+#/usr/bin/sudo -n ghc-pkg recache
 
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
