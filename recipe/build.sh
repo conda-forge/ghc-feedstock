76c2ef58ed3601230be573e4e80653c43c1f1a7d
step21 <step21@devtal.de>
2020-10-06 12:49:58 +0200

    fixed build mk

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,13 +27,16 @@ if [ $ARCH == "64" ]; then
 fi
 echo "Targetplatform"
 echo $TARGETPLATFORM
+
 cp $BUILD_PREFIX/mk/build.mk.sample $BUILD_PREFIX/mk/build.mk
+ls $PWD
+ls $BUILD_PREFIX
 ./boot
 # from eggzilla
 # ./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$PREFIX/bin/cpp --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$PREFIX/bin/cc LD=$PREFIX/bin/cc AR=$PREFIX/bin/ar AS=$BUILD_PREFIX/bin/as CFLAGS=-fno-builtin CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold LD_NO_GOLD=$PREFIX/bin/cc
 #./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 if [ $ARCH == "64" ]; then
-  ./configure --prefix=$PREFIX CC=$PREFIX/bin/cc
+:  ./configure --prefix=$PREFIX CC=$PREFIX/bin/cc
   #./configure --prefix=$PREFIX CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 fi
 if [ $ARCH == "aarch64" ]; then
@@ -42,7 +45,6 @@ fi
 ls $PWD
 ls $BUILD_PREFIX
 # from eggzilla
-cp mk/build.mk.sample mk/build.mk
 cp $BUILD_PREFIX/mk/build.mk.sample $BUILD_PREFIX/mk/build.mk
 perl -pi -e 's/#BuildFlavour = quick\n/BuildFlavour = quickest\n/' mk/build.mk
 #echo "V=0" >> mk/build.mk
