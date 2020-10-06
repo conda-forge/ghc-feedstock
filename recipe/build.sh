afd5a3c4008247d1a8a1291e8553b5fc710e8936
step21 <step21@devtal.de>
2020-10-06 11:47:44 +0200

    fixed build mk

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,7 +27,7 @@ if [ $ARCH == "64" ]; then
 fi
 echo "Targetplatform"
 echo $TARGETPLATFORM
-cp build.mk $BUILD_PREFIX/mk/
+cp $BUILD_PREFIX/mk/build.mk.sample $BUILD_PREFIX/mk/build.mk
 ./boot
 # from eggzilla
 # ./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$PREFIX/bin/cpp --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$PREFIX/bin/cc LD=$PREFIX/bin/cc AR=$PREFIX/bin/ar AS=$BUILD_PREFIX/bin/as CFLAGS=-fno-builtin CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold LD_NO_GOLD=$PREFIX/bin/cc
