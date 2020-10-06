7bcda66a0e071ea1e0c324bc95aefcee40bad83d
step21 <step21@devtal.de>
2020-10-06 10:05:41 +0200

    changed configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,17 +3,9 @@ unset build_alias
 export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
 #export CFLAGS="-I$PREFIX/include $CFLAGS"
 #export LDFLAGS="-Wl,-L$PREFIX/lib $LDFLAGS"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+#export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+#export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 # from eggzilla/ghc-feedstock
-echo "prefix"
-ls $PREFIX
-echo "prefix bin"
-ls $PREFIX/bin
-echo "prefix lib"
-ls $PREFIX/lib
-#ls $PREFIX/lib/gcc
-#ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld.gold $PREFIX/bin/ld.gold
 #ln -s $PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $PREFIX/bin/ld
 #echo "ls $PREFIX/lib"
 #ls $PREFIX/lib
@@ -22,8 +14,8 @@ ls $PREFIX/lib
 #ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu
 #ls $PREFIX/lib/gcc/x86_64-conda_cos6-linux-gnu/7.3.0
 
-export LD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
-export LD_NO_GOLD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
+#export LD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
+#export LD_NO_GOLD=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 
 if [ $ARCH == "aarch64" ]; then
    echo "aarch64 detected"
@@ -33,8 +25,7 @@ if [ $ARCH == "64" ]; then
   echo "x86_64 detected"
   export TARGETPLATFORM="x86_64-unknown-linux"
 fi
-uname -a
-which perl
+echo "Targetplatform"
 echo $TARGETPLATFORM
 #cp build.mk $BUILD_PREFIX/mk/
 ./boot
@@ -42,7 +33,8 @@ echo $TARGETPLATFORM
 # ./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$PREFIX/bin/cpp --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$PREFIX/bin/cc LD=$PREFIX/bin/cc AR=$PREFIX/bin/ar AS=$BUILD_PREFIX/bin/as CFLAGS=-fno-builtin CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold LD_NO_GOLD=$PREFIX/bin/cc
 #./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 if [ $ARCH == "64" ]; then
-  ./configure --prefix=$PREFIX CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
+  ./configure
+  #./configure --prefix=$PREFIX CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
 fi
 if [ $ARCH == "aarch64" ]; then
   ./configure --prefix=$BUILD_PREFIX --with-gmp-includes=$BUILD_PREFIX/include --with-gmp-libraries=$BUILD_PREFIX/lib
