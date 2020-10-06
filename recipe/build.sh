0be1ac38f86be2d585ffb4f62c0d7addaa7563fe
step21 <step21@devtal.de>
2020-10-06 21:21:18 +0200

    which make

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,6 +40,8 @@ echo "booting ghc"
 # from eggzilla
 # ./configure --prefix=$PREFIX --enable-bootstrap-with-devel-snapshot --with-ffi-includes=$PREFIX/include --with-ffi-libraries=$PREFIX/lib --with-system-libffi CPP=$PREFIX/bin/cpp --with-gmp-includes=$PREFIX/include --with-curses-libraries=$PREFIX/lib --with-gmp-libraries=$PREFIX/lib LDFLAGS=-L$PREFIX/lib CC=$PREFIX/bin/cc LD=$PREFIX/bin/cc AR=$PREFIX/bin/ar AS=$BUILD_PREFIX/bin/as CFLAGS=-fno-builtin CONF_GCC_LINKER_OPTS_STAGE0=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE1=-fuse-ld=gold CONF_GCC_LINKER_OPTS_STAGE2=-fuse-ld=gold LD_NO_GOLD=$PREFIX/bin/cc
 #./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+echo "ARCH"
+echo $ARCH
 if [ $ARCH == "64" ]; then
 :  ./configure --prefix=$PREFIX CC=$PREFIX/bin/cc
   #./configure --prefix=$PREFIX CPP=x86_64-conda_cos6-linux-gnu-cpp --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc LD=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
@@ -56,7 +58,8 @@ echo "trying to copy build config"
 cp $PWD/mk/build.mk.sample $PWD/mk/build.mk
 perl -pi -e 's/#BuildFlavour = quick\n/BuildFlavour = quickest\n/' mk/build.mk
 #echo "V=0" >> mk/build.mk
-#
+echo "make"
+which make
 make
 make install
 #iif [[ -f "$LD" && ! $BUILD_PREFIX/bin/ld ]]; then
