60771578ce5a5fbc77013e93cc3f031a1ba2d790
Uwe L. Korn <uwe.korn@quantco.com>
2021-05-12 15:26:21 +0200

    ghc 9.2.1-alpha2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -exuo pipefail
+
 unset host_alias
 unset build_alias
 unset CFLAGS
@@ -5,33 +9,52 @@ unset CXXFLAGS
 unset CPPFLAGS
 unset LDFLAGS
 
-export GHC_BUILD=$(echo $BUILD | sed "s/conda/unknown/g")
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  export GHC_BUILD=x86_64-apple-darwin
+else
+  export GHC_BUILD=$(echo $BUILD | sed "s/conda/unknown/g")
+fi
 export GHC_HOST=$(echo $HOST | sed "s/conda/unknown/g")
 
-# Make sure libraries for build are found without LDFLAGS
-cp $BUILD_PREFIX/lib/libgmp.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
-cp $BUILD_PREFIX/lib/libncurses.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
-cp $BUILD_PREFIX/lib/libtinfo.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
+if [[ "${target_platform}" == linux-* ]]; then
+  # Make sure libraries for build are found without LDFLAGS
+  cp $BUILD_PREFIX/lib/libgmp.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
+  cp $BUILD_PREFIX/lib/libncurses.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
+  cp $BUILD_PREFIX/lib/libtinfo.so $BUILD_PREFIX/$BUILD/sysroot/usr/lib/
 
-# Make sure libraries for host are found without LDFLAGS
-cp $PREFIX/lib/libgmp.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
-cp $PREFIX/lib/libncurses.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
-cp $PREFIX/lib/libtinfo.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
+  # Make sure libraries for host are found without LDFLAGS
+  cp $PREFIX/lib/libgmp.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
+  cp $PREFIX/lib/libncurses.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
+  cp $PREFIX/lib/libtinfo.so $BUILD_PREFIX/$HOST/sysroot/usr/lib/
 
-# workaround some bugs in autoconf scripts
-cp $(which $AR) $BUILD_PREFIX/bin/$GHC_HOST-ar
-cp $(which $GCC) $BUILD_PREFIX/bin/$GHC_HOST-gcc
+  # workaround some bugs in autoconf scripts
+  cp $(which $AR) $BUILD_PREFIX/bin/$GHC_HOST-ar
+  cp $(which $GCC) $BUILD_PREFIX/bin/$GHC_HOST-gcc
+fi
 
 pushd binary
+  cp $BUILD_PREFIX/share/gnuconfig/config.* .
   # stage0 compiler: --build=$GHC_BUILD --host=$GHC_BUILD --target=$GHC_BUILD
-  LDFLAGS="-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib -Wl,-rpath-link,$BUILD_PREFIX/lib" CC=$BUILD-gcc AR=$BUILD-ar LD=$BUILD-ld NM=$BUILD-nm OBJDUMP=$BUILD-objdump RANLIB=$BUILD-ranlib CPP=$BUILD-cpp STRIP=$BUILD-strip ./configure --prefix=$BUILD_PREFIX --with-gmp-includes=$BUILD_PREFIX/include --with-gmp-libraries=$BUILD_PREFIX/lib --build=$GHC_BUILD --host=$GHC_BUILD --target=$GHC_BUILD
-  make install #-j${CPU_COUNT}
+  (
+    LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    CC=${CC_FOR_BUILD:-$CC}
+    AR=($CC -print-prog-name=ar)
+    NM=($CC -print-prog-name=nm)
+    if [[ "${build_platform}" == linux-* ]]; then
+      CPP=$BUILD-cpp
+    fi
+    LD=$BUILD-ld OBJDUMP=$BUILD-objdump RANLIB=$BUILD-ranlib STRIP=$BUILD-strip ./configure --prefix=$BUILD_PREFIX --with-gmp-includes=$BUILD_PREFIX/include --with-gmp-libraries=$BUILD_PREFIX/lib --build=$GHC_BUILD --host=$GHC_BUILD --target=$GHC_BUILD
+    make install #-j${CPU_COUNT}
+  )
 popd
 
 pushd source
   # stage1 compiler: --build=$GHC_BUILD --host=$GHC_BUILD --target=$GHC_HOST
   # stage2 compiler: --build=$GHC_BUILD --host=$GHC_HOST --target=$GHC_HOST
-  export CC=$GCC
+  if [[ "${target_platform}" == linux-* ]]; then
+    export CC=$GCC
+  fi
+  cp $BUILD_PREFIX/share/gnuconfig/config.* .
   ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib --build=$GHC_BUILD --host=$GHC_BUILD --target=$GHC_HOST
   make HADDOCK_DOCS=NO BUILD_SPHINX_HTML=NO BUILD_SPHINX_PDF=NO #-j${CPU_COUNT}
   make HADDOCK_DOCS=NO BUILD_SPHINX_HTML=NO BUILD_SPHINX_PDF=NO install #-j${CPU_COUNT}
