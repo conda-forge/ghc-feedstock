7b01a64c708034bcfabb1a60a6b0529a734309c6
Uwe L. Korn <uwe.korn@quantco.com>
2021-10-28 14:06:46 +0200

    Skipping ghc-pkg recache during ghc* package build

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-conda_target_arch-ghc-pkg-PKG_VERSION recache
+if [[ "${CONDA_BUILD:-0}" == 1 && "${PKG_NAME}" == ghc* ]]; then
+  echo "Skipping ghc-pkg recache during ghc* package build"
+  export
+else
+  conda_target_arch-ghc-pkg-PKG_VERSION recache
+fi
 export GHC=${CONDA_PREFIX}/bin/conda_target_arch-ghc-PKG_VERSION
 export GHC_PKG=${CONDA_PREFIX}/bin/conda_target_arch-ghc-pkg-PKG_VERSION
 export HSC2HS=${CONDA_PREFIX}/bin/conda_target_arch-hsc2hs
