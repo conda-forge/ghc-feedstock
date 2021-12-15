1275bf50a3b2984e36886533c18acd502189d6e0
Uwe L. Korn <uwe.korn@quantco.com>
2021-12-15 21:16:19 +0100

    Skip activation in package build

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-conda_target_arch-ghc-pkg-PKG_VERSION recache
+if [[ "${PKG_NAME:-}" != "ghc_osx-arm64" ]]; then
+  conda_target_arch-ghc-pkg-PKG_VERSION recache
+fi
 export GHC=${CONDA_PREFIX}/bin/conda_target_arch-ghc-PKG_VERSION
 export GHC_PKG=${CONDA_PREFIX}/bin/conda_target_arch-ghc-pkg-PKG_VERSION
 export HSC2HS=${CONDA_PREFIX}/bin/conda_target_arch-hsc2hs
