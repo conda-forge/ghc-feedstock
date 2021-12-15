d7e06ebcbbcb0084c3a1468a482b5286541e8d5f
Uwe L. Korn <uwe.korn@quantco.com>
2021-12-15 17:09:36 +0100

    Revert "Skipping ghc-pkg recache during ghc* package build"
    This reverts commit 7b01a64c708034bcfabb1a60a6b0529a734309c6.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [[ "${CONDA_BUILD:-0}" == 1 && "${PKG_NAME}" == ghc* ]]; then
-  echo "Skipping ghc-pkg recache during ghc* package build"
-  export
-else
-  conda_target_arch-ghc-pkg-PKG_VERSION recache
-fi
+conda_target_arch-ghc-pkg-PKG_VERSION recache
 export GHC=${CONDA_PREFIX}/bin/conda_target_arch-ghc-PKG_VERSION
 export GHC_PKG=${CONDA_PREFIX}/bin/conda_target_arch-ghc-pkg-PKG_VERSION
 export HSC2HS=${CONDA_PREFIX}/bin/conda_target_arch-hsc2hs
