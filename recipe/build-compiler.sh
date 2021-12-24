57d07e57a3a0e0cad1eb9325b4718b16de6cb384
Uwe L. Korn <uwe.korn@quantco.com>
2021-12-24 07:39:49 +0100

    Set GhcLibHcOpts

--- recipe/build-compiler.sh
+++ recipe/build-compiler.sh
@@ -132,13 +132,16 @@ pushd source
     #else
     #  sed 's/#\(BuildFlavour = quick\)/\1/' mk/build.mk.sample > mk/build.mk
     #fi
+
     sed 's/#\(BuildFlavour = quick\)/\1/' mk/build.mk.sample > mk/build.mk
+    echo "GhcLibHcOpts       = -O2" >> mk/build.mk
     if [[ "${ghc_target_platform}" != "${target_platform}" ]]; then
       echo 'Stage1Only = YES' >> mk/build.mk
     fi
     if [[ "${target_platform}" == osx-* ]]; then
       echo "DYNAMIC_GHC_PROGRAMS = NO" >> mk/build.mk
     fi
+
     export CONF_CC_OPTS_STAGE0="${CFLAGS}"
     export CONF_CC_OPTS_STAGE1="${CFLAGS_GHC_TARGET}"
     # FIXME: Does this work?
