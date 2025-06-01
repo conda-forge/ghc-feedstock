fac22ece06e511c97b1a1900fa9fe96d766217ac
memento <claude.rc@gmail.com>
2025-06-01 17:15:02 -0500

    (deb) merge ld for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -ex
 unset host_alias
 
 # Set environment variables
-export MergeObjsCmd=${LD_GOLD}
+export MergeObjsCmd=${LD_GOLD:-${LD}}
 export CC=${CC}
 export CXX=${CXX}
 export M4=${BUILD_PREFIX}/bin/m4
@@ -12,21 +12,30 @@ export PYTHON=${BUILD_PREFIX}/bin/python
 
 # Set up binary directory
 mkdir -p binary _logs
+_log_index=0
 
 # Install bootstrap GHC - Set conda platform moniker
 pushd bootstrap-ghc
-    CC="${CC_FOR_BUILD}" \
-    CXX="${CXX_FOR_BUILD}" \
-    ./configure \
-    --prefix="${PWD}"/../binary #> ../_logs/bs-configure.log 2>&1
-  make install #> ../_logs/bs-make-install.log 2>&1
-popd
+  CC="${CC_FOR_BUILD}" \
+  CXX="${CXX_FOR_BUILD}" \
+  LDFLAGS="${LDFLAGS//$PREFIX/$BUILD_PREFIX}" \
+  _logname="bs-configure"
+  ./configure \
+    --prefix="${SRC_DIR}"/binary > ../_logs/${_log_index}_${_logname}.log 2>&1; tail -20 ../_logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  _logname="bs-make-install" make install > ../_logs/${_log_index}_${_logname}.log 2>&1; tail -20 ../_logs/${_log_index}_${_logname}.log; let "_log_index += 1"
 
-if [[ -d target-ghc-libs ]]; then
-  pushd target-ghc-libs
-    ./configure --prefix="${PWD}"/../binary --target="${GHC_TARGET}" #> ../_logs/bs-libs-configure.log 2>&1
-  popd
-fi
+  # Find bootstrap HShaskeline and HSterminfo
+  find "${SRC_DIR}/binary" -type f -name "*HShaskeline*.so" -o -name "*HSterminfo*.so" | while read lib; do
+    # Check if this library has the RPATH we want to change
+    current_rpath=$(patchelf --print-rpath "$lib" 2>/dev/null)
+    if [[ "$current_rpath" == *"$PREFIX/lib"* ]]; then
+      # Replace with BUILD_PREFIX but preserve any other paths
+      new_rpath="${current_rpath//$PREFIX\/lib/$BUILD_PREFIX/lib}"
+      patchelf --set-rpath "$new_rpath" "$lib"
+      patchelf --replace-needed libtinfo.so.6 "$BUILD_PREFIX"/lib/libtinfo.so.6 "$lib"
+    fi
+  done
+popd
 
 # Add binary GHC to PATH
 export PATH=$PWD/binary/bin:$PATH
@@ -36,9 +45,9 @@ mkdir -p binary/bin
 cp bootstrap-cabal/cabal binary/bin/
 
 # Update cabal package database
-cabal v2-update > _logs/cabal-configure.log 2>&1
+_logname="cabal-update" cabal v2-update > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
 
-case "$target_platform" in
+case "${target_platform}" in
   linux-*)
     GHC_BUILD=x86_64-unknown-linux
     GHC_HOST=x86_64-unknown-linux
@@ -74,10 +83,20 @@ CONFIGURE_ARGS=(
 )
 
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-./configure "${CONFIGURE_ARGS[@]}" #> _logs/configure.log 2>&1
+_logname="ghc-configure" ./configure "${CONFIGURE_ARGS[@]}" > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
 
 # Build and install using hadrian
-hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --freeze1 --docs=no-sphinx-pdfs
+_logname="stage1_exe" hadrian/build stage1:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+_logname="stage1_lib" hadrian/build stage1:lib:ghc -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+_logname="stage2_exe" hadrian/build stage2:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+_logname="stage2_lib" hadrian/build stage2:lib:ghc -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+_logname="build_all" hadrian/build -j"${CPU_COUNT}" --flavour=release --freeze1 --freeze2 --docs=no-sphinx-pdfs --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+
+if [[ -n "${CROSSCOMPILING_EMULATOR:-}" ]]; then
+  _logname="install" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+else
+  _logname="install_xcompiled" CC="${CC_FOR_BUILD}" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+fi
 
 # Create bash completion
 mkdir -p "${PREFIX}"/etc/bash_completion.d
@@ -89,19 +108,7 @@ rm -f "${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/package.conf.d/package.cache.lock
 
 # Run post-install
 if [[ -n "${CROSSCOMPILING_EMULATOR:-}" ]]; then
-  "${PREFIX}"/bin/ghc-pkg recache
+  _logname="recache_xcompiled" "${CROSSCOMPILING_EMULATOR}" "${PREFIX}"/bin/ghc-pkg recache > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
 else
-  "${CROSSCOMPILING_EMULATOR}" "${PREFIX}"/bin/ghc-pkg recache
+  _logname="recache" "${PREFIX}"/bin/ghc-pkg recache > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
 fi
-
-# # For macOS, fix library paths if needed
-# if [[ "$target_platform" == "osx-"* ]]; then
-#   find "${PREFIX}/lib" -name "*.dylib" -o -name "*.so" | while read lib; do
-#     install_name_tool -change "@rpath/libgmp.10.dylib" "${PREFIX}/lib/libgmp.10.dylib" "$lib" || true
-#   done
-#
-#   # Also fix executables
-#   find "${PREFIX}/bin" -type f -executable | while read exe; do
-#     install_name_tool -change "@rpath/libgmp.10.dylib" "${PREFIX}/lib/libgmp.10.dylib" "$exe" || true
-#   done
-# fi
