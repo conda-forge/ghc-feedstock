ac336b2a433e0e3b6fd4f5edbe4f10dbadb03a40
memento <claude.rc@gmail.com>
2025-06-01 21:50:54 -0500

    (dbg) new logs. arm64 BS. win pkgs

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,50 @@
 #!/usr/bin/env bash
-set -ex
+set -eu
+
+_log_index=0
+
+# Function to run a command, log its output, and increment log index
+run_and_log() {
+  local _logname="$1"
+  shift
+  local cmd=("$@")
+
+  echo "Running: ${cmd[*]}"
+  "${cmd[@]}" > "${SRC_DIR}/_logs/${_log_index}_${_logname}.log" 2>&1 &
+  local cmd_pid=$!
+  # Counter to track when to display tail output
+  local tail_counter=0
+  # Periodically flush and show progress
+  while kill -0 $cmd_pid 2>/dev/null; do
+    sync
+    echo -n "."
+    sleep 10
+    let "tail_counter += 1"
+    # After 3 cycles (30 seconds), show log tail and reset counter
+    if [ $tail_counter -ge 3 ]; then
+      echo "."
+      tail -5 "${SRC_DIR}/_logs/${_log_index}_${_logname}.log"
+      tail_counter=0
+    fi
+  done
+  wait $cmd_pid
+  local exit_code=$?
+
+  echo ".";echo ".";echo ".";echo "."
+  printf "[--- %s ---]" "${cmd[*]}"
+  tail -50 "${SRC_DIR}/_logs/${_log_index}_${_logname}.log"
+  echo "[---------------------------------------]"
+  echo ".";echo ".";echo ".";echo "."
+
+  let "_log_index += 1"
+
+  if [[ $exit_code -ne 0 ]]; then
+    echo "Command failed with exit code $exit_code"
+    return $exit_code
+  fi
+
+  return 0
+}
 
 unset host_alias
 
@@ -12,29 +57,28 @@ export PYTHON=${BUILD_PREFIX}/bin/python
 
 # Set up binary directory
 mkdir -p binary _logs
-_log_index=0
 
 # Install bootstrap GHC - Set conda platform moniker
 pushd bootstrap-ghc
   CC="${CC_FOR_BUILD}" \
   CXX="${CXX_FOR_BUILD}" \
   LDFLAGS="${LDFLAGS//$PREFIX/$BUILD_PREFIX}" \
-  _logname="bs-configure"
-  ./configure \
-    --prefix="${SRC_DIR}"/binary > ../_logs/${_log_index}_${_logname}.log 2>&1; tail -20 ../_logs/${_log_index}_${_logname}.log; let "_log_index += 1"
-  _logname="bs-make-install" make install > ../_logs/${_log_index}_${_logname}.log 2>&1; tail -20 ../_logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  run_and_log "bs-configure" ./configure --prefix="${SRC_DIR}"/binary
+  run_and_log "bs-make-install" make install
 
-  # Find bootstrap HShaskeline and HSterminfo
-  find "${SRC_DIR}/binary" -type f -name "*HShaskeline*.so" -o -name "*HSterminfo*.so" | while read lib; do
-    # Check if this library has the RPATH we want to change
-    current_rpath=$(patchelf --print-rpath "$lib" 2>/dev/null)
-    if [[ "$current_rpath" == *"$PREFIX/lib"* ]]; then
-      # Replace with BUILD_PREFIX but preserve any other paths
-      new_rpath="${current_rpath//$PREFIX\/lib/$BUILD_PREFIX/lib}"
-      patchelf --set-rpath "$new_rpath" "$lib"
+  if [[ "${build_platform}" == "linux-64" ]]; then
+    # Find bootstrap HShaskeline and HSterminfo
+    find "${SRC_DIR}/binary" -type f -name "*HShaskeline*.so" -o -name "*HSterminfo*.so" | while read lib; do
+      # Check if this library has the RPATH we want to change
+      current_rpath=$(patchelf --print-rpath "$lib" 2>/dev/null)
+      patchelf --set-rpath "$BUILD_PREFIX/lib" "$lib"
+      if [[ -n "$current_rpath" ]]; then
+        patchelf --add-rpath "$current_rpath" "$lib"
+      fi
       patchelf --replace-needed libtinfo.so.6 "$BUILD_PREFIX"/lib/libtinfo.so.6 "$lib"
-    fi
-  done
+      readelf -d "$lib"
+    done
+  fi
 popd
 
 # Add binary GHC to PATH
@@ -45,7 +89,7 @@ mkdir -p binary/bin
 cp bootstrap-cabal/cabal binary/bin/
 
 # Update cabal package database
-_logname="cabal-update" cabal v2-update > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+run_and_log "cabal-update" cabal v2-update
 
 case "${target_platform}" in
   linux-*)
@@ -82,20 +126,19 @@ CONFIGURE_ARGS=(
   --with-curses-libraries="${PREFIX}"/lib
 )
 
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-_logname="ghc-configure" ./configure "${CONFIGURE_ARGS[@]}" > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+run_and_log "ghc-configure" ./configure "${CONFIGURE_ARGS[@]}"
 
 # Build and install using hadrian
-_logname="stage1_exe" hadrian/build stage1:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
-_logname="stage1_lib" hadrian/build stage1:lib:ghc -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
-_logname="stage2_exe" hadrian/build stage2:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
-_logname="stage2_lib" hadrian/build stage2:lib:ghc -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
-_logname="build_all" hadrian/build -j"${CPU_COUNT}" --flavour=release --freeze1 --freeze2 --docs=no-sphinx-pdfs --progress-info=none > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+run_and_log "stage1_exe" hadrian/build stage1:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none
+run_and_log "stage1_lib" hadrian/build stage1:lib:ghc -j"${CPU_COUNT}" --flavour=release --docs=none --progress-info=none
+run_and_log "stage2_exe" hadrian/build stage2:exe:ghc-bin -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none
+run_and_log "stage2_lib" hadrian/build stage2:lib:ghc -j"${CPU_COUNT}" --flavour=release --freeze1 --docs=none --progress-info=none
+run_and_log "build_all"  hadrian/build -j"${CPU_COUNT}" --flavour=release --freeze1 --freeze2 --docs=no-sphinx-pdfs --progress-info=none
 
 if [[ -n "${CROSSCOMPILING_EMULATOR:-}" ]]; then
-  _logname="install" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  run_and_log "install" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs
 else
-  _logname="install_xcompiled" CC="${CC_FOR_BUILD}" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  run_and_log "install_xcompiled" CC="${CC_FOR_BUILD}" hadrian/build install -j"${CPU_COUNT}" --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs
 fi
 
 # Create bash completion
@@ -108,7 +151,7 @@ rm -f "${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/package.conf.d/package.cache.lock
 
 # Run post-install
 if [[ -n "${CROSSCOMPILING_EMULATOR:-}" ]]; then
-  _logname="recache_xcompiled" "${CROSSCOMPILING_EMULATOR}" "${PREFIX}"/bin/ghc-pkg recache > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  run_and_log "recache_xcompiled" "${CROSSCOMPILING_EMULATOR}" "${PREFIX}"/bin/ghc-pkg recache
 else
-  _logname="recache" "${PREFIX}"/bin/ghc-pkg recache > _logs/${_log_index}_${_logname}.log 2>&1; tail -20 _logs/${_log_index}_${_logname}.log; let "_log_index += 1"
+  run_and_log "recache" "${PREFIX}"/bin/ghc-pkg recache
 fi
