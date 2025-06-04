9ecce1bb6bc8c54d2fd3d848106a91ec7c04a7e8
memento <claude.rc@gmail.com>
2025-06-04 17:38:15 -0500

    (fix) looks like 9.12.2 cannot build itself :/

--- recipe/building/build-osx-64.sh
+++ recipe/building/build-osx-64.sh
@@ -21,7 +21,7 @@ pushd bootstrap-ghc
 popd
 
 # Update cabal package database
-run_and_log "cabal-update" cabal v2-update
+run_and_log "cabal-update" cabal v2-update --allow-newer --minimize-conflict-set
 
 # Configure and build GHC
 # SYSTEM_CONFIG=(
