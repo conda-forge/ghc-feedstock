204c05e8daf89be6ad0876c02e2c03563fad1f84
mementorc <claude.rc@gmail.com>
2025-06-11 21:01:07 -0500

    (dev) correct installed settings

--- recipe/building/build-linux-aarch64.sh
+++ recipe/building/build-linux-aarch64.sh
@@ -130,3 +130,4 @@ pushd "${PREFIX}"/share/doc/aarch64-linux-ghc-"${PKG_VERSION}"-inplace
     cp "${file///-}" "${SRC_DIR}"/license_files
   done
 popd
+perl -pi -e 's#($ENV{BUILD_PREFIX}|$ENV{PREFIX})/bin/##' "${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/settings
