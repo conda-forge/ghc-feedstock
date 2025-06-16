b5d514191746792ad086827ce51f6ab213fdc23f
mementorc <claude.rc@gmail.com>
2025-06-16 14:09:30 -0500

    (fix) no freeze, stage1

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -8,8 +8,16 @@ source "${RECIPE_DIR}"/building/common.sh
 export PYTHON=python
 export MSYSTEM=MINGW64
 
+ls -lrt bootstrap-ghc/*
+
 # Update cabal package database
 run_and_log "cabal-update" cabal v2-update
+# run_and_log "cabal-install" cabal install -j \
+#   --installdir=/usr/local/bin \
+#   --install-method=copy \
+#   ghc-platform-0.1.0.0\
+#   hadrian-0.1.0.0 \
+#   alex-3.2.6 happy hscolour
 
 _hadrian_build=("${SRC_DIR}"/hadrian/build.bat "-j")
 
@@ -36,4 +44,5 @@ CONFIGURE_ARGS=(
   --with-iconv-libraries="${PREFIX}"/lib
 )
 run_and_log "ghc-configure" bash configure "${SYSTEM_CONFIG[@]}" "${CONFIGURE_ARGS[@]}"
-run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --flavour=release --freeze1 --freeze2 --docs=no-sphinx-pdfs
+run_and_log "stage1_exe" "${_hadrian_build[@]}" stage1:exe:ghc-bin -VV --flavour=release --docs=none --progress-info=unicorn
+run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --flavour=release --docs=none
