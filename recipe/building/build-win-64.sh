0ca8621e6781479765997830e4e59b4768b22b82
mementorc <claude.rc@gmail.com>
2025-06-15 20:21:45 -0500

    (fix) add PYTHON export

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -5,7 +5,7 @@ _log_index=0
 
 source "${RECIPE_DIR}"/building/common.sh
 
-export PYTHON=${BUILD_PREFIX}/bin/python.exe
+export PYTHON=${BUILD_PREFIX}/Scripts/python.exe
 export MSYSTEM=MINGW64
 
 # Update cabal package database
