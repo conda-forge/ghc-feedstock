f908cc75f6622101a06d7a305aecca8aa5064c1c
mementorc <claude.rc@gmail.com>
2025-06-23 21:43:50 -0500

    (fix) .

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -84,41 +84,42 @@ pushd libraries/directory
   mkdir -p "${BUILD_PREFIX}"/bin
   ln -s "${BUILD_PREFIX}"/Library/usr/bin/m4.exe "${BUILD_PREFIX}"/bin
 
-  AR_STAGE0=llvm-ar \
-  CC=clang \
-  CC_STAGE0=clang \
-  CFLAGS="${CFLAGS//-nostdlib/}" \
-  CXX=clang++ \
-  CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
-  LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
-  MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
-  MergeObjsArgs="" \
-  run_and_log "directory-autoreconf" bash autoreconf -fi
+  # AR_STAGE0=llvm-ar \
+  # CC=clang \
+  # CC_STAGE0=clang \
+  # CFLAGS="${CFLAGS//-nostdlib/}" \
+  # CXX=clang++ \
+  # CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
+  # LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
+  # MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
+  # MergeObjsArgs="" \
+  # run_and_log "directory-autoreconf" bash autoreconf -fi
 
-  AR_STAGE0=llvm-ar \
-  CC=clang \
-  CC_STAGE0=clang \
-  CFLAGS="${CFLAGS//-nostdlib/}" \
-  CXX=clang++ \
-  CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
-  LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
-  MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
-  MergeObjsArgs="" \
-  run_and_log "directory-configure" bash configure
+  # AR_STAGE0=llvm-ar \
+  # CC=clang \
+  # CC_STAGE0=clang \
+  # CFLAGS="${CFLAGS//-nostdlib/}" \
+  # CXX=clang++ \
+  # CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
+  # LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
+  # MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
+  # MergeObjsArgs="" \
+  # run_and_log "directory-configure" bash configure
 
-  AR_STAGE0=llvm-ar \
-  CC=clang \
-  CC_STAGE0=clang \
-  CFLAGS="${CFLAGS//-nostdlib/}" \
-  CXX=clang++ \
-  CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
-  LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
-  MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
-  MergeObjsArgs="" \
+  # AR_STAGE0=llvm-ar \
+  # CC=clang \
+  # CC_STAGE0=clang \
+  # CFLAGS="${CFLAGS//-nostdlib/}" \
+  # CXX=clang++ \
+  # CXXFLAGS="${CXXFLAGS//-nostdlib/}" \
+  # LDFLAGS="${LDFLAGS//-nostdlib/} -Wl,-defaultlib:msvcrt -Wl,-defaultlib:oldnames" \
+  # MergeObjsCmd="x86_64-w64-mingw32-ld.exe" \
+  # MergeObjsArgs="" \
   cabal build \
     --verbose=3 \
     --with-compiler="${SRC_DIR}"/bootstrap-ghc/bin/ghc.exe \
-    --with-gcc="${BUILD_PREFIX}"/bin/clang.exe
+    --with-gcc="${BUILD_PREFIX}"Library/bin/clang.exe \
+    --with-llvm-ar="${BUILD_PREFIX}"Library/bin/llvm-ar.exe
 popd
 
 "${_hadrian_build[@]}" stage1:exe:ghc-bin -VV \
