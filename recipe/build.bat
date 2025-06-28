b441636515771947833ab5ccd31c47ce56ebda5d
mementorc <claude.rc@gmail.com>
2025-06-28 17:53:40 -0500

    .

--- recipe/build.bat
+++ recipe/build.bat
@@ -10,11 +10,13 @@ type "%RECIPE_DIR%\build.sh"                          >> conda_build.sh
 set _PREFIX=%PREFIX:\=/%
 set _BUILD_PREFIX=%BUILD_PREFIX:\=/%
 set _SRC_DIR=%SRC_DIR:\=/%
-for %%D in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    set "_PREFIX=!_PREFIX:%%D:=/%%D:~0,1,1!"
-    set "_BUILD_PREFIX=!_BUILD_PREFIX:%%D:=/%%D:~0,1,1!"
-    set "_SRC_DIR=!_SRC_DIR:%%D:=/%%D:~0,1,1!"
-)
+set _PREFIX=%_PREFIX:C:=/c/%
+set _BUILD_PREFIX=%_BUILD_PREFIX:C:=/c/%
+set _SRC_DIR=%_SRC_DIR:C:=/c/%
+set _PREFIX=%_PREFIX:D:=/d/%
+set _BUILD_PREFIX=%_BUILD_PREFIX:D:=/d/%
+set _SRC_DIR=%_SRC_DIR:D:=/d/%
+
 set MSYSTEM=MINGW64
 set MSYS2_PATH_TYPE=inherit
 set MSYS2_ARG_CONV_EXCL="*"
