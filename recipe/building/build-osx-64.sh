1583a8d69f994297287c9bfc7b94e2aef828090a
mementorc <claude.rc@gmail.com>
2025-06-28 14:42:52 -0500

    MNT: v9.12.2 (unix)

---
+++ recipe/building/build-osx-64.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eu
+
+_log_index=0
+
+source "${RECIPE_DIR}"/building/common.sh
+
+# Install bootstrap GHC - Set conda platform moniker
+pushd bootstrap-ghc
+  run_and_log "bs-configure" bash configure \
+    --prefix="${SRC_DIR}"/binary \
+    --enable-ghc-toolchain
+  cp default.target.ghc-toolchain default.target
+  run_and_log "bs-make-install" make install
+popd
+
+# Update cabal package database
+run_and_log "cabal-update" cabal v2-update --allow-newer --minimize-conflict-set
+
+# Configure and build GHC
+SYSTEM_CONFIG=(
+  --build="x86_64-apple-darwin13.4.0"
+  --host="x86_64-apple-darwin13.4.0"
+)
+
+CONFIGURE_ARGS=(
+  --prefix="${PREFIX}"
+  --disable-numa
+  --enable-ignore-build-platform-mismatch=yes
+  --with-system-libffi=yes
+  --with-curses-includes="${PREFIX}"/include
+  --with-curses-libraries="${PREFIX}"/lib
+  --with-ffi-includes="${PREFIX}"/include
+  --with-ffi-libraries="${PREFIX}"/lib
+  --with-gmp-includes="${PREFIX}"/include
+  --with-gmp-libraries="${PREFIX}"/lib
+  --with-iconv-includes="${PREFIX}"/include
+  --with-iconv-libraries="${PREFIX}"/lib
+)
+
+run_and_log "ghc-configure" bash configure "${SYSTEM_CONFIG[@]}" "${CONFIGURE_ARGS[@]}"
+_hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
+run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --flavour=release --docs=no-sphinx-pdfs
+cat "${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/settings
