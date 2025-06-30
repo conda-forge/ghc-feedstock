4d5ca8b715c6a9bc0e8a80e61c73fe8615410325
mementorc <claude.rc@gmail.com>
2025-06-30 17:43:46 -0500

    (fix) .

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -27,7 +27,8 @@ fi
 find "${_BUILD_PREFIX}" -name "*clang_rt.builtins*"
 
 # Define the wrapper script for MSVC
-CLANG_WRAPPER="${BUILD_PREFIX}\\Library\\bin\\clang-mingw-wrapper.ps1"
+CLANG_WRAPPER="${BUILD_PREFIX}\\Library\\bin\\clang-mingw-wrapper.bat"
+cp "${RECIPE_DIR}/building/clang-mingw-wrapper.bat" "${_BUILD_PREFIX}/Library/bin/"
 cp "${RECIPE_DIR}/building/clang-mingw-wrapper.ps1" "${_BUILD_PREFIX}/Library/bin/"
 
 # Make sure we use conda-forge clang (ghc bootstrap has a clang.exe)
--- recipe/building/clang-mingw-wrapper.bat
+++ recipe/building/clang-mingw-wrapper.bat
@@ -1,93 +1,12 @@
 @echo on
-setlocal enabledelayedexpansion
+setlocal
 
-echo [WRAPPER] Starting clang-mingw-wrapper 1>&2
+REM This batch file acts as a bridge to execute the PowerShell wrapper
+echo [WRAPPER] Starting PowerShell wrapper bridge 1>&2
 echo [WRAPPER] Arguments: %* 1>&2
 
-set "filtered_args="
+REM Execute the PowerShell script via powershell.exe
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%BUILD_PREFIX%\Library\bin\clang-mingw-wrapper.ps1" %*
 
-REM Check if we're dealing with a response file
-for %%a in (%*) do (
-    set "arg=%%a"
-
-    REM Special handling for response files (starting with @)
-    if "!arg:~0,1!"=="@" (
-        echo [WRAPPER] Found response file: !arg! 1>&2
-        set "resp_file=!arg:~1!"
-
-        REM Create a temporary filtered response file
-        set "temp_resp=%TEMP%\filtered_rsp_%RANDOM%.txt"
-        echo [WRAPPER] Creating filtered response file: !temp_resp! 1>&2
-
-        REM Process the response file line by line
-        if exist "!resp_file!" (
-            for /f "usebackq delims=" %%i in ("!resp_file!") do (
-                set "rline=%%i"
-                set "skip_line=0"
-
-                REM Only filter -I and -L flags containing mingw/bootstrap
-                if "!rline:~0,2!"=="-I" (
-                    if not "!rline:mingw=!"=="!rline!" set "skip_line=1"
-                    if not "!rline:bootstrap=!"=="!rline!" set "skip_line=1"
-                ) else if "!rline:~0,2!"=="-L" (
-                    if not "!rline:mingw=!"=="!rline!" set "skip_line=1"
-                    if not "!rline:bootstrap=!"=="!rline!" set "skip_line=1"
-                )
-
-                if "!skip_line!"=="1" (
-                    echo [WRAPPER] Skipping line from response file: !rline! 1>&2
-                ) else (
-                    REM Preserve all other flags including -D_UCRT
-                    echo !rline!>>"!temp_resp!"
-                )
-            )
-
-            REM Use the filtered response file
-            set "filtered_args=!filtered_args! "@!temp_resp!""
-        ) else (
-            echo [WRAPPER] Warning: Response file !resp_file! not found 1>&2
-            set "filtered_args=!filtered_args! "!arg!""
-        )
-    ) else (
-        REM Handle normal arguments
-        set "skip=0"
-
-        if "!arg:~0,2!"=="-I" (
-            if not "!arg:mingw=!"=="!arg!" set "skip=1"
-            if not "!arg:bootstrap=!"=="!arg!" set "skip=1"
-        ) else if "!arg:~0,2!"=="-L" (
-            if not "!arg:mingw=!"=="!arg!" set "skip=1"
-            if not "!arg:bootstrap=!"=="!arg!" set "skip=1"
-        )
-
-        if "!skip!"=="1" (
-            echo [WRAPPER] Skipping: !arg! 1>&2
-        ) else (
-            set "filtered_args=!filtered_args! "!arg!""
-        )
-    )
-)
-
-REM Add conda mingw paths (not to response file)
-echo [WRAPPER] Adding conda mingw paths 1>&2
-set "filtered_args=!filtered_args! "-I%BUILD_PREFIX%\Library\mingw-w64\include" "-L%BUILD_PREFIX%\Library\mingw-w64\lib" "-L%BUILD_PREFIX%\Library\mingw-w64\lib""
-
-REM Improved search for builtins library
-set "builtins_found=0"
-for /f "delims=" %%a in ('dir /s /b "%BUILD_PREFIX%\*clang*\*\lib\*\*clang_rt.builtins*.lib" "%BUILD_PREFIX%\*clang*\*\lib\*\*clang_rt.builtins*.a" 2^>nul') do (
-    set "builtins=%%a"
-    set "builtins_found=1"
-    echo [WRAPPER] Found builtins: !builtins! 1>&2
-    goto :found
-)
-:found
-
-if "!builtins_found!"=="1" (
-    echo [WRAPPER] Using direct path to builtins library 1>&2
-    echo [WRAPPER] Final command: "%BUILD_PREFIX%\Library\bin\clang.exe" !filtered_args! --target=x86_64-w64-mingw32 -fuse-ld=lld -rtlib=compiler-rt "!builtins!" 1>&2
-    "%BUILD_PREFIX%\Library\bin\clang.exe" !filtered_args! --target=x86_64-w64-mingw32 -fuse-ld=lld -rtlib=compiler-rt "!builtins!"
-) else (
-    echo [WRAPPER] Falling back to library search 1>&2
-    echo [WRAPPER] Final command: "%BUILD_PREFIX%\Library\bin\clang.exe" !filtered_args! --target=x86_64-w64-mingw32 -fuse-ld=lld -rtlib=compiler-rt -lclang_rt.builtins-x86_64 1>&2
-    "%BUILD_PREFIX%\Library\bin\clang.exe" !filtered_args! --target=x86_64-w64-mingw32 -fuse-ld=lld -rtlib=compiler-rt -lclang_rt.builtins-x86_64
-)
+REM Pass along the exit code
+exit /b %ERRORLEVEL%
