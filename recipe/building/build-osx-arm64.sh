520ee5a23e0bdbfdd0e3cf928fcec475b5402b9c
mementorc <claude.rc@gmail.com>
2025-07-30 11:35:15 -0500

    (dbg) SDK path?

--- recipe/building/build-osx-arm64.sh
+++ recipe/building/build-osx-arm64.sh
@@ -74,7 +74,7 @@ fi
 _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
 
 settings_file=$(find "${PREFIX}"/ghc-bootstrap -name settings | head -1)
-if [[ "${target_platform}" == "osx-"* ]] && [[ -n "${SDKROOT}" ]]; then
+if [[ -n "${SDKROOT}" ]]; then
   perl -i -pe 's#("C compiler link flags", ")([^"]*)"#\1\2 -L$ENV{SDKROOT}/usr/lib"#g' "${settings_file}"
 fi
 "${_hadrian_build[@]}" stage1:exe:ghc-bin -vv --progress-info=unicorn
