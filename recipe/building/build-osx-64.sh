0a319eebc718e8d09a46a3caab203e283103bb90
mementorc <claude.rc@gmail.com>
2025-09-10 13:43:33 -0500

    (fix) SDK iostream?

--- recipe/building/build-osx-64.sh
+++ recipe/building/build-osx-64.sh
@@ -40,6 +40,7 @@ export CXXFLAGS="-stdlib=libc++ ${CXXFLAGS:-}"
 export CXXFLAGS="-isysroot $(xcrun --show-sdk-path) ${CXXFLAGS:-}"
 export CXXFLAGS="-isystem ${BUILD_PREFIX}/include/c++/v1 ${CXXFLAGS:-}"
 export CXX="${CXX:-clang++} -stdlib=libc++ -v -isystem ${BUILD_PREFIX}/include/c++/v1 -isysroot $(xcrun --show-sdk-path)"
+export ac_cv_cxx_stdlib_flavour="c++"
 run_and_log "ghc-configure" bash configure "${SYSTEM_CONFIG[@]}" "${CONFIGURE_ARGS[@]}"
 
 _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
