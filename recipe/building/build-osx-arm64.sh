f584ef826830d011c5e4b98c25ac3fa49eb579ef
mementorc <claude.rc@gmail.com>
2025-09-22 14:44:38 -0500

    (fix) odd gmp/iconv not found. ar/ranlib

--- recipe/building/build-osx-arm64.sh
+++ recipe/building/build-osx-arm64.sh
@@ -70,9 +70,13 @@ if [[ "${_build_alias}" != "${_host_alias}" ]]; then
   perl -pi -e 's#"--target=[\w-]+"#"--target=x86_64-apple-darwin"#'  "${SRC_DIR}"/hadrian/cfg/default.host.target
   perl -pi -e 's/aarch64/x86_64/;s/ArchAArch64/ArchX86_64/' "${SRC_DIR}"/hadrian/cfg/default.host.target
 
-  perl -i -pe 's#/usr/bin/ar("[^"]*"q)cls#x86_64-apple-darwin13.4.0-ar$1#g' "${SRC_DIR}"/hadrian/cfg/default.host.target
+  perl -i -pe 's#/usr/bin/ar("[^"]*"q)cls#x86_64-apple-darwin13.4.0-ar${1}s#g' "${SRC_DIR}"/hadrian/cfg/default.host.target
   perl -i -pe 's#((arIsGnu|arSupportsAtFile) = )False#$1True#g' "${SRC_DIR}"/hadrian/cfg/default.host.target
   perl -i -pe 's#(arNeedsRanlib = )True#$1False#g' "${SRC_DIR}"/hadrian/cfg/default.host.target
+
+  perl -i -pe 's#/usr/bin/ar("[^"]*"q)cls#x86_64-apple-darwin13.4.0-ar${1}s#g' "${SRC_DIR}"/hadrian/cfg/default.target
+  perl -i -pe 's#((arIsGnu|arSupportsAtFile) = )False#$1True#g' "${SRC_DIR}"/hadrian/cfg/default.target
+  perl -i -pe 's#(arNeedsRanlib = )True#$1False#g' "${SRC_DIR}"/hadrian/cfg/default.target
 fi
 
 echo "*"; echo "*"; echo "*"; echo "*"; 
@@ -88,7 +92,7 @@ _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
 # ld: warning: ignoring file /Users/runner/.local/state/cabal/store/ghc-9.6.7/tf8-strng-1.0.2-7159478e/lib/libHStf8-strng-1.0.2-7159478e.a, building for macOS-x86_64 but attempting to link with file built for unknown-unsupported file format ( 0x21 0x3C 0x61 0x72 0x63 0x68 0x3E 0x0A 0x2F 0x20 0x20 0x20 0x20 0x20 0x20 0x20 )
 settings_file=$(find "${BUILD_PREFIX}"/ghc-bootstrap -name settings | head -1)
 perl -i -pe 's#("ar command", ")([^"]*)"#\1 x86_64-apple-darwin13.4.0-ar"#g' "${settings_file}"
-perl -i -pe 's#("ar flags", ")([^"]*)"#\1q"#g' "${settings_file}"
+perl -i -pe 's#("ar flags", ")([^"]*)"#\1qs"#g' "${settings_file}"
 perl -i -pe 's#("ranlib command", ")([^"]*)"#\1 x86_64-apple-darwin13.4.0-ranlib"#g' "${settings_file}"
 
 cat "${settings_file}"
