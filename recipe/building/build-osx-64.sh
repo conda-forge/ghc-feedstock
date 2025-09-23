0edcd9f8b17ba1945aabc52e1416da2fc9d3d709
mementorc <claude.rc@gmail.com>
2025-09-23 08:37:29 -0500

    (fix) going backward :(

--- recipe/building/build-osx-64.sh
+++ recipe/building/build-osx-64.sh
@@ -54,28 +54,17 @@ perl -i -pe 's#/usr/bin/ar("[^"]*"q)cls#x86_64-apple-darwin13.4.0-ar${1}s#g' "${
 perl -i -pe 's#((arIsGnu|arSupportsAtFile) = )False#$1True#g' "${settings_file}"
 perl -i -pe 's#(arNeedsRanlib = )True#$1False#g' "${settings_file}"
 
-echo "*"; echo "*"; echo "*"; echo "*"
-cat "${settings_file}"
-echo "*"; echo "*"; echo "*"; echo "*"
-
 _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
 
-run_and_log "stage1_exe" "${_hadrian_build[@]}" stage1:exe:ghc-bin --flavour=quickest
+"${_hadrian_build[@]}" -V stage1:exe:ghc-bin --flavour=quickest
 settings_file="${SRC_DIR}"/_build/stage0/lib/settings
 perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -v -Wl,-L$ENV{PREFIX}/lib -Wl,-L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib#' "${settings_file}"
 perl -pi -e 's#(ld flags", "[^"]*)#$1 -v -L$ENV{PREFIX}/lib -L\$topdir/../../../../lib -rpath \$topdir/../../../../lib#' "${settings_file}"
-echo "*"; echo "*"; echo "*"; echo "*"
-cat "${settings_file}"
-echo "*"; echo "*"; echo "*"; echo "*"
-
 
 run_and_log "stage1_lib" "${_hadrian_build[@]}" stage1:lib:ghc --flavour=quickest
 perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -Wl,-L\$ENV{PREFIX}/lib -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
 perl -i -pe "s#(ld flags\", \")([^\"]*)#\1\2 -L\$ENV{PREFIX}/lib -L\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
 
-echo "*"; echo "*"; echo "*"; echo "*"
-cat "${settings_file}"
-echo "*"; echo "*"; echo "*"; echo "*"
 run_and_log "stage2_exe" "${_hadrian_build[@]}" stage2:exe:ghc-bin --flavour=quickest
 
 export DYLD_LIBRARY_PATH="${PREFIX}/lib:${BUILD_PREFIX}/lib:${DYLD_LIBRARY_PATH:-}"
