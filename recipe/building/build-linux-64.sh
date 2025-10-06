cefb621a756007e6a44ebba57defafdb59ed5d9b
mementorc <claude.rc@gmail.com>
2025-10-06 12:33:06 -0500

    (fix) restore full static/dynamic release

--- recipe/building/build-linux-64.sh
+++ recipe/building/build-linux-64.sh
@@ -33,7 +33,7 @@ CONFIGURE_ARGS=(
 )
 run_and_log "ghc-configure" bash configure "${SYSTEM_CONFIG[@]}" "${CONFIGURE_ARGS[@]}"
 
-export CABFLAGS=(--enable-shared --disable-static --disable-library-vanilla --enable-executable-dynamic -j)
+export CABFLAGS=(--enable-shared --enable-executable-dynamic -j)
 for pkg in hadrian; do
   (cd  "${SRC_DIR}"/hadrian/ && ${CABAL} v2-build "${CABFLAGS[@]}" --ghc-options="-dynamic -shared -fPIC -optl-dynamic -optl-Wl,-rpath,${PREFIX}/lib -optl-L${PREFIX}/lib -optl-liconv -optl-lffi -optl-lgmp -optl-ltinfo -optl-ltinfow" "${pkg}")
 done
@@ -44,51 +44,33 @@ export LIBRARY_PATH="${BUILD_PREFIX}/lib:${PREFIX}/lib${LIBRARY_PATH:+:}${LIBRAR
 export LD_LIBRARY_PATH="${BUILD_PREFIX}/lib:${PREFIX}/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-}"
 
 settings_file="${SRC_DIR}"/_build/stage0/lib/settings
-perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib#' "${settings_file}"
-perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -L\$topdir/../../../../lib -rpath \$topdir/../../../../lib#' "${settings_file}"
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
 run_and_log "stage1_lib" "${_hadrian_build[@]}" stage1:lib:ghc --flavour=release
 
 perl -pi -e 's#(C compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
 perl -pi -e 's#(C\+\+ compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
-perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{PREFIX}/lib -Wl,-L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib#' "${settings_file}"
-perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{PREFIX}/lib -L\$topdir/../../../../lib -rpath \$topdir/../../../../lib#' "${settings_file}"
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
-export LD_PRELOAD="${PREFIX}/lib/libiconv.so.2 ${PREFIX}/lib/libgmp.so.10 ${PREFIX}/lib/libffi.so.8 ${PREFIX}/lib/libtinfow.so.6 ${PREFIX}/lib/libtinfo.so.6 ${LD_PRELOAD:-}"
+# export LD_PRELOAD="${PREFIX}/lib/libiconv.so.2 ${PREFIX}/lib/libgmp.so.10 ${PREFIX}/lib/libffi.so.8 ${PREFIX}/lib/libtinfow.so.6 ${PREFIX}/lib/libtinfo.so.6 ${LD_PRELOAD:-}"
 
 run_and_log "stage2_exe" "${_hadrian_build[@]}" stage2:exe:ghc-bin --flavour=release --freeze1
 
 settings_file="${SRC_DIR}"/_build/stage1/lib/settings
 perl -pi -e 's#(C compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
 perl -pi -e 's#(C\+\+ compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
-perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{PREFIX}/lib -Wl,-L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib#' "${settings_file}"
-perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{PREFIX}/lib -L\$topdir/../../../../lib -rpath \$topdir/../../../../lib#' "${settings_file}"
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
 run_and_log "stage2_lib" "${_hadrian_build[@]}" stage2:lib:ghc --flavour=release --freeze1
 run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --freeze1 --freeze2 --flavour=release --docs=none
 
 installed_settings="${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/settings
-if [ -f "${installed_settings}" ]; then
+if [[ -f "${installed_settings}" ]]; then
   echo "Fixing installed settings file with RPATH..."
-  perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-rpath,\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib#" "${installed_settings}"
-  perl -pi -e "s#(ld flags\", \")#\$1-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -rpath \\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -L\\\$topdir/../../../../lib -rpath \\\$topdir/../../../../lib#" "${installed_settings}"
+  perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-rpath,\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib#" "${installed_settings}"
+  perl -pi -e "s#(ld flags\", \")#\$1-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -rpath \\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -L\\\$topdir/../../../lib -rpath \\\$topdir/../../../lib#" "${installed_settings}"
 fi
 
-# Remove the .a links (we're dynamic-only)
-find "${PREFIX}"/lib/ghc-"${PKG_VERSION}" -name "*.a" -exec rm {} \;
-
-# CRITICAL: Create .so symlinks for dynamic linking
-# GHC generates library files like libHSbase-4.18.3.0-ghc9.6.7.so
-# But linker flags use -lHSbase-4.18.3.0 which looks for libHSbase-4.18.3.0.so
-# We need symlinks from short names to versioned names
-echo "Creating .so symlinks for dynamic linking..."
-find "${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/x86_64-linux-ghc-"${PKG_VERSION}" -name "*.so" -type f | while read -r fullpath; do
-  dir=$(dirname "$fullpath")
-  fullname=$(basename "$fullpath")
-  # Remove -ghc9.6.7 suffix to get short name
-  shortname=${fullname%-ghc${PKG_VERSION}.so}.so
-  if [ "$shortname" != "$fullname" ]; then
-    echo "  Creating symlink: $shortname -> $fullname"
-    ln -sf "$fullname" "$dir/$shortname"
-  fi
-done
--- recipe/building/build-osx-64.sh
+++ recipe/building/build-osx-64.sh
@@ -12,9 +12,8 @@ export CABAL_DIR="${SRC_DIR}/.cabal"
 unset build_alias
 unset host_alias
 
-# Update cabal package database
 mkdir -p "${CABAL_DIR}" && "${CABAL}" user-config init
-run_and_log "cabal-update" cabal v2-update
+run_and_log "cabal-update" "${CABAL}" v2-update
 
 _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
 
@@ -40,14 +39,31 @@ CONFIGURE_ARGS=(
 
 export ac_cv_path_ac_pt_CC=""
 export ac_cv_path_ac_pt_CXX=""
+export ac_cv_prog_AR="${AR}"
+export ac_cv_prog_CC="${CC}"
+export ac_cv_prog_CXX="${CXX}"
+export ac_cv_prog_LD="${LD}"
+export ac_cv_prog_RANLIB="${RANLIB}"
+export ac_cv_path_AR="${AR}"
+export ac_cv_path_CC="${CC}"
+export ac_cv_path_CXX="${CXX}"
+export ac_cv_path_LD="${LD}"
+export ac_cv_path_RANLIB="${RANLIB}"
+export DEVELOPER_DIR=""
+
+settings_file=$(find "${BUILD_PREFIX}"/ghc-bootstrap -name settings | head -n 1)
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -v -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -v -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib#' "${settings_file}"
+set_macos_conda_ar_ranlib "${settings_file}" "${CONDA_TOOLCHAIN_BUILD}"
+
 run_and_log "configure" bash configure "${SYSTEM_CONFIG[@]}" "${CONFIGURE_ARGS[@]}"
 
-export CABFLAGS=(--enable-shared --disable-static --disable-library-vanilla --enable-executable-dynamic -j)
+export CABFLAGS=(--enable-shared --enable-executable-dynamic -j)
 for pkg in hadrian; do
   (cd  "${SRC_DIR}"/hadrian/ && ${CABAL} v2-build "${CABFLAGS[@]}" --ghc-options="-dynamic -shared -fPIC -optl-dynamic -optl-Wl,-rpath,${PREFIX}/lib -optl-L${PREFIX}/lib -optl-liconv -optl-lffi -optl-lgmp -optl-ltinfo -optl-ltinfow" "${pkg}")
 done
 
-run_and_log "stage1_exe" "${_hadrian_build[@]}" stage1:exe:ghc-bin --flavour=quickest
+run_and_log "stage1_exe" "${_hadrian_build[@]}" stage1:exe:ghc-bin --flavour=release
 
 export LIBRARY_PATH="${BUILD_PREFIX}/lib:${PREFIX}/lib${LIBRARY_PATH:+:}${LIBRARY_PATH:-}"
 export DYLD_LIBRARY_PATH="${BUILD_PREFIX}/lib:${PREFIX}/lib${DYLD_LIBRARY_PATH:+:}${DYLD_LIBRARY_PATH:-}"
@@ -56,35 +72,33 @@ iconv_aliases="-Wl,-alias,_libiconv,_iconv"
 iconv_aliases="${iconv_aliases} -Wl,-alias,_libiconv_open,_iconv_open"
 iconv_aliases="${iconv_aliases} -Wl,-alias,_libiconv_close,_iconv_close"
 settings_file="${SRC_DIR}"/_build/stage0/lib/settings
-perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -v -Wl,-L$ENV{PREFIX}/lib -Wl,-L\$topdir/../../../../lib -Wl,-rpath,\$topdir/../../../../lib#' "${settings_file}"
-perl -pi -e 's#(ld flags", "[^"]*)#$1 -v -L$ENV{BUILD_REFIX}/lib -L\$topdir/../../../../lib -rpath \$topdir/../../../../lib#' "${settings_file}"
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
 run_and_log "stage1_lib" "${_hadrian_build[@]}" stage1:lib:ghc --flavour=release
 
-perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -Wl,-L\$ENV{PREFIX}/lib -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-perl -i -pe "s#(ld flags\", \")([^\"]*)#\1\2 -L\$ENV{PREFIX}/lib -L\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
+perl -pi -e 's#(C compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
+perl -pi -e 's#(C\+\+ compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
-export DYLD_INSERT_LIBRARIES=$(find "${PREFIX}" -name libtinfow.dylib)
+# export LD_PRELOAD="${PREFIX}/lib/libiconv.so.2 ${PREFIX}/lib/libgmp.so.10 ${PREFIX}/lib/libffi.so.8 ${PREFIX}/lib/libtinfow.so.6 ${PREFIX}/lib/libtinfo.so.6 ${LD_PRELOAD:-}"
+
+run_and_log "stage2_exe" "${_hadrian_build[@]}" stage2:exe:ghc-bin --flavour=release --freeze1
+
 settings_file="${SRC_DIR}"/_build/stage1/lib/settings
 perl -pi -e 's#(C compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
 perl -pi -e 's#(C\+\+ compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
-perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -Wl,-L\$ENV{PREFIX}/lib -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-perl -i -pe "s#(ld flags\", \")([^\"]*)#\1\2 -L\$ENV{PREFIX}/lib -L\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-run_and_log "stage2_exe" "${_hadrian_build[@]}" stage2:exe:ghc-bin --flavour=release
+perl -pi -e 's#(C compiler link flags", "[^"]*)#$1 -Wl,-L$ENV{BUILD_PREFIX}/lib -Wl,-L$ENV{PREFIX}/lib -Wl,-rpath,$ENV{BUILD_PREFIX}/lib -Wl,-rpath,$ENV{PREFIX}/lib#' "${settings_file}"
+perl -pi -e 's#(ld flags", "[^"]*)#$1 -L$ENV{BUILD_PREFIX}/lib -L$ENV{PREFIX}/lib -rpath $ENV{PREFIX}/lib -rpath $ENV{BUILD_PREFIX}/lib#' "${settings_file}"
 
-perl -pi -e 's#(C compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
-perl -pi -e 's#(C\+\+ compiler flags", "[^"]*)#$1 -Wno-strict-prototypes#' "${settings_file}"
-perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -Wl,-L\$ENV{PREFIX}/lib -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-perl -i -pe "s#(ld flags\", \")([^\"]*)#\1\2 -L\$ENV{PREFIX}/lib -L\\\$topdir/../../../../lib ${iconv_aliases} -liconv#" "${settings_file}"
-run_and_log "stage2_lib" "${_hadrian_build[@]}" stage2:lib:ghc --flavour=release
-run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --flavour=release --docs=none --progress-info=none
+run_and_log "stage2_lib" "${_hadrian_build[@]}" stage2:lib:ghc --flavour=release --freeze1
+run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --freeze1 --freeze2 --flavour=release --docs=none
 
 installed_settings="${PREFIX}"/lib/ghc-"${PKG_VERSION}"/lib/settings
-if [ -f "${installed_settings}" ]; then
+if [[ -f "${installed_settings}" ]]; then
   echo "Fixing installed settings file with RPATH..."
-  perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/../x86_64-linux-ghc-${PKG_VERSION} -Wl,-rpath,\\\$topdir/../x86_64-linux-ghc-${PKG_VERSION} -Wl,-L\\\$topdir/../../../../lib -Wl,-rpath,\\\$topdir/../../../../lib#" "${installed_settings}"
-  perl -pi -e "s#(ld flags", ")#\$1-L\\\$topdir/../x86_64-linux-ghc-${PKG_VERSION} -rpath \\\$topdir/../x86_64-linux-ghc-${PKG_VERSION} -L\\\$topdir/../../../../lib -rpath \\\$topdir/../../../../lib#" "${installed_settings}"
+  perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-rpath,\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib#" "${installed_settings}"
+  perl -pi -e "s#(ld flags\", \")#\$1-L\\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -rpath \\\$topdir/x86_64-linux-ghc-${PKG_VERSION} -L\\\$topdir/../../../lib -rpath \\\$topdir/../../../lib#" "${installed_settings}"
 fi
 
-# Remove the .a links
-find "${PREFIX}"/lib/ghc-"${PKG_VERSION}" -name "*.a" -exec rm {} \;
