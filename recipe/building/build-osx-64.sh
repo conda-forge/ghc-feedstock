c2f7ff4bb8948a2af736e9f80bfe2cc150e6c473
mementorc <claude.rc@gmail.com>
2025-10-15 20:24:08 -0500

    (dbg) use lld

--- recipe/building/build-osx-64.sh
+++ recipe/building/build-osx-64.sh
@@ -105,9 +105,13 @@ run_and_log "stage2_lib" "${_hadrian_build[@]}" stage2:lib:ghc --flavour=quick -
 run_and_log "install" "${_hadrian_build[@]}" install --flavour=quick --prefix="${PREFIX}" --freeze1 --freeze2 --docs=none --progress-info=none
 
 settings_file=$(find "${PREFIX}" -name settings | head -n 1)
+
+# Create symlink so clang can find ld64.lld as just "ld64.lld"
+ln -sf "${PREFIX}"/bin/ld64.lld "${PREFIX}"/bin/ld 2>/dev/null || true
+
 perl -i -pe "s#(C compiler flags\", \")([^\"]*)#\1\2 -fno-lto#" "${settings_file}"
 perl -i -pe "s#(C\\+\\+ compiler flags\", \")([^\"]*)#\1\2 -fno-lto#" "${settings_file}"
-perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -fno-lto -fno-use-linker-plugin -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib -liconv -Wl,-L\\\$topdir/../lib -Wl,-rpath,\\\$topdir/../lib -liconv_compat#" "${settings_file}"
+perl -i -pe "s#(C compiler link flags\", \")([^\"]*)#\1\2 -v -fuse-ld=ld64.lld -fno-lto -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib -liconv -Wl,-L\\\$topdir/../lib -Wl,-rpath,\\\$topdir/../lib -liconv_compat#" "${settings_file}"
 perl -i -pe "s#\"(llc|opt|clang)\"#\"x86_64-apple-darwin13.4.0-\1\"#" "${settings_file}"
 set_macos_conda_ar_ranlib "${settings_file}" "${CONDA_TOOLCHAIN_BUILD}"
 
