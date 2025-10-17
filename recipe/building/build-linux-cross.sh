c8987ede3157408996dfd26ca1144543a67ba507
mementorc <claude.rc@gmail.com>
2025-10-17 14:35:17 -0500

    (fix) add dyn profiling. simpler linux cross

--- recipe/building/build-linux-cross.sh
+++ recipe/building/build-linux-cross.sh
@@ -175,6 +175,7 @@ if [[ -f "${settings_file}" ]]; then
   perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib#" "${settings_file}"
   perl -pi -e "s#(ld flags\", \"[^\"]*)#\$1 -L\\\$topdir/../../../lib -rpath \\\$topdir/../../../lib#" "${settings_file}"
   perl -pi -e "s#\"[/\w]*?(ar|clang|clang\+\+|ld|ranlib|llc|opt)\"#\"${conda_target}-\$1\"#" "${settings_file}"
+  cat "${settings_file}"
 else
   echo "Error: Could not find settins file"
   exit 1
