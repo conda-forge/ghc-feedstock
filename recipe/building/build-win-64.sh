ef883688240210ca8927f848b3b9f37cfc92bfdc
mementorc <claude.rc@gmail.com>
2025-11-12 10:47:21 -0600

    (dbg) Try Clang/LLVM

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -124,9 +124,13 @@ export CXX_STD_LIB_LIBS="stdc++"
 
 # Configure Clang for MinGW cross-compilation
 # CRITICAL: Clang needs explicit target, sysroot, and include paths
-export CFLAGS="-I${BUILD_PREFIX}/Library/include -I/C/Program Files\ \(x86\)/Windows\ Kits/10/Include/10.0.26100.0/um -I${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/include ${CFLAGS:-}"
-export CXXFLAGS="-I${BUILD_PREFIX}/Library/include -I/C/Program Files\ \(x86\)/Windows\ Kits/10/Include/10.0.26100.0/um -I${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/include ${CXXFLAGS:-}"
-export LDFLAGS="-nostdlib -L${BUILD_PREFIX}/Library/lib -L/c/Program Files\ \(x86\)/Windows\ Kits/10/Lib/10.0.26100.0/um/x64 -L${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/lib"
+UCRT_INCLUDE=$(cygpath -u 'C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/ucrt')
+UM_INCLUDE=$(cygpath -u 'C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/um')
+UM_LIB=$(cygpath -u 'C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/x64')
+  
+export CFLAGS="-I${BUILD_PREFIX}/Library/include -I${UCRT_INCLUDE} -I${UM_INCLUDE} -I${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/include ${CFLAGS:-}"
+export CXXFLAGS="-I${BUILD_PREFIX}/Library/include -I${UCRT_INCLUDE} -I${UM_INCLUDE} -I${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/include ${CXXFLAGS:-}"
+export LDFLAGS="-nostdlib -L${BUILD_PREFIX}/Library/lib -L${UM_LIB} -L${BUILD_PREFIX}/Library/x86_64-w64-mingw32/sysroot/usr/lib"
 
 (
   CFLAGS_CONFIGURE=$(echo "${CFLAGS}" | sed 's/-nostdlib//g' | sed 's/--target=x86_64-w64-mingw32//g')
