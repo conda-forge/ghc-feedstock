0acba4a079aaab1c663351f420277c3c6918321c
mementorc <claude.rc@gmail.com>
2025-11-14 21:01:57 -0600

    (dbg) Try Clang/LLVM

--- recipe/building/build-win-64.sh
+++ recipe/building/build-win-64.sh
@@ -187,11 +187,12 @@ if [ -d "${CLANG_RESOURCE_DIR}/lib" ]; then
 else
   echo "WARNING: ${CLANG_RESOURCE_DIR}/lib does not exist"
 fi
-export CFLAGS="--target=x86_64-w64-mingw32 -rtlib= -unwindlib=none -D__MINGW32__ -D_VA_LIST_DEFINED -D__GNUC__=13 -Dva_list=__builtin_va_list -isystem ${CLANG_BUILTIN_INCLUDE} -isystem ${_BUILD_PREFIX}/Library/include -isystem ${MINGW_SYSROOT}/usr/include ${CFLAGS:-}"
-export CXXFLAGS="--target=x86_64-w64-mingw32 -rtlib= -unwindlib=none -D__MINGW32__ -D_VA_LIST_DEFINED -D__GNUC__=13 -Dva_list=__builtin_va_list -isystem ${CLANG_BUILTIN_INCLUDE} -isystem ${_BUILD_PREFIX}/Library/include -isystem ${MINGW_SYSROOT}/usr/include ${CXXFLAGS:-}"
+export CFLAGS="--target=x86_64-w64-mingw32 -D__MINGW32__ -D_VA_LIST_DEFINED -D__GNUC__=13 -Dva_list=__builtin_va_list -isystem ${CLANG_BUILTIN_INCLUDE} -isystem ${_BUILD_PREFIX}/Library/include -isystem ${MINGW_SYSROOT}/usr/include ${CFLAGS:-}"
+export CXXFLAGS="--target=x86_64-w64-mingw32 -D__MINGW32__ -D_VA_LIST_DEFINED -D__GNUC__=13 -Dva_list=__builtin_va_list -isystem ${CLANG_BUILTIN_INCLUDE} -isystem ${_BUILD_PREFIX}/Library/include -isystem ${MINGW_SYSROOT}/usr/include ${CXXFLAGS:-}"
 # Let Clang's driver handle linking - it will automatically use appropriate linker and libraries
 # We only need to specify library search paths and additional libraries
-export LDFLAGS="-L${_BUILD_PREFIX}/Library/lib -L${_BUILD_PREFIX}/Library/mingw-w64/lib -L${MINGW_SYSROOT}/usr/lib ${LDFLAGS}"
+# -nodefaultlibs: Don't link standard system libraries (we'll add what we need explicitly)
+export LDFLAGS="-nodefaultlibs -L${_BUILD_PREFIX}/Library/lib -L${_BUILD_PREFIX}/Library/mingw-w64/lib -L${MINGW_SYSROOT}/usr/lib ${LDFLAGS}"
 # MinGW libraries that Clang might not add automatically
 # CRITICAL: Find and explicitly link compiler-rt builtins library
 # Try both .a (MinGW format) and .lib (MSVC format) extensions
